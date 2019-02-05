a21fced3cac9d0cce1de894e72d9e77452ce10b5
Chris Hoffman <99742+chrishoffman@users.noreply.github.com>
2019-02-05 12:33:52 -0500

    moving log file

--- assets/bin/scanner.sh
+++ assets/bin/scanner.sh
@@ -7,17 +7,17 @@ while true; do
             filename=`basename $file`
             mv -f $file "/data/av/scan/${filename}"
             printf "Processing /data/av/scan/${filename}\n"
-            /usr/local/bin/scanfile.sh > /data/av/scan/info 2>&1
+            /usr/local/bin/scanfile.sh > /data/av/info 2>&1
             if [ -e "/data/av/scan/${filename}" ]
             then
                 printf "  --> File ok\n"
                 mv -f "/data/av/scan/${filename}" "/data/av/ok/${filename}"
-                mv -f "/data/av/scan/info" "/data/av/ok/${filename}.log"
+                mv -f "/data/av/info" "/data/av/ok/${filename}.log"
                 printf "  --> File moved to /data/av/ok/${filename}\n"
             elif [ -e "/data/av/quarantine/${filename}" ]
             then
                 printf "  --> File quarantined / nok\n"
-                mv -f "/data/av/scan/info" "/data/av/nok/${filename}"
+                mv -f "/data/av/info" "/data/av/nok/${filename}"
                 printf "  --> Scan report moved to /data/av/nok/${filename}\n"
             fi
         fi
